19322e12d7bb985e8fcc4004d456c9684bc4c87f
ayaz.ali <ayaz.ali@netpace.com>
2014-12-03 13:12:29 +0500

    Introducing file syncing features on specific directory on a local system. Feature is supplemented by Apache VFS

---
+++ src/main/java/edu/nu/cs/utils/FileChangeListener.java
@@ -0,0 +1,82 @@
+package edu.nu.cs.utils;
+
+import org.apache.commons.vfs2.*;
+import org.apache.commons.vfs2.impl.SynchronizedFileObject;
+
+/**
+ * @author Ayaz Ali Qureshi
+ * @version 1.0
+ * @see org.apache.commons.vfs2.FileListener
+ * <p/>
+ * Implementation of FileListener, this class is basically responsible for copying and deleting files once a change is detected in the
+ * corresponding filesystem
+ */
+public class FileChangeListener implements FileListener {
+
+    private FileSystemManager fsManager;
+    private SynchronizedFileObject cwd;
+    private SynchronizedFileObject dest;
+    private SynchronizedFileObject src;
+
+    /**
+     * Initialize all variables
+     *
+     * @throws FileSystemException
+     */
+    public FileChangeListener() throws FileSystemException {
+        this.fsManager = VFS.getManager();
+        this.cwd = new SynchronizedFileObject(fsManager.resolveFile(Properties.baseDirectory));
+        this.dest = new SynchronizedFileObject(fsManager.resolveFile(cwd, Properties.destinationDirectory));
+        this.src = new SynchronizedFileObject(fsManager.resolveFile(cwd, Properties.sourceDirectory));
+
+    }
+
+    /**
+     * Invoke copy operation from SRC ==> DEST
+     *
+     * @param event
+     * @throws Exception
+     */
+    @Override
+    public void fileCreated(org.apache.commons.vfs2.FileChangeEvent event) throws Exception {
+
+        String fileName = UtilityClass.getRelPathToFile(event.getFile().getName().getFriendlyURI());
+        System.out.println("file created : " + UtilityClass.getRelPathToFile(event.getFile().getName().getFriendlyURI()));
+        dest.copyFrom(src, Selectors.SELECT_ALL);
+    }
+
+    /**
+     * Delete file if-exists SRC ==> DEST
+     *
+     * @param event
+     * @throws Exception
+     */
+    @Override
+    public void fileDeleted(org.apache.commons.vfs2.FileChangeEvent event) throws Exception {
+        System.out.println("file deleted : "
+                + event.getFile().getName());
+
+        String fileName = UtilityClass.getRelPathToFile(event.getFile().getName().getFriendlyURI());
+        SynchronizedFileObject newDest = new SynchronizedFileObject(fsManager.resolveFile(cwd, Properties.destinationDirectory + fileName));
+        if (newDest.exists()) {
+            newDest.delete(Selectors.SELECT_SELF);
+        }
+
+    }
+
+    /**
+     * Invoke copy operation from SRC ==> DEST
+     *
+     * @param event
+     * @throws Exception
+     */
+    @Override
+    public void fileChanged(org.apache.commons.vfs2.FileChangeEvent event) throws Exception {
+        System.out.println(String.format(
+                "File [%s] changed event from [%s]", event
+                        .getFile().getName(), this));
+
+        System.out.println("file created : " + UtilityClass.getRelPathToFile(event.getFile().getName().getFriendlyURI()));
+        dest.copyFrom(src, Selectors.SELECT_ALL);
+    }
+}
---
+++ src/main/webapp/views/monitorStarted.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Hasnain
+  Date: 11/26/14
+  Time: 3:01 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>File Monitor Started ...</h1>
+</body>
+</html>
